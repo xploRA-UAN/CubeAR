aef363cc80e546216ec18e1db57e80c49832553c
Razhor <zamorano0695@gmail.com>
2019-11-14 12:06:37 -0700

    Upload Files CubeAR

---
+++ Assets/Scripts/ChangeScenes.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScenes : MonoBehaviour{
+
+    public void Camera()
+    {
+        SceneManager.LoadScene("Camera");
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+    
+}
